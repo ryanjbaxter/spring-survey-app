d739ea08a2d8c306453ec92fda7cd3a250a0e453
Ryan Baxter <ryan.baxter@broadcom.com>
2026-02-17 15:04:01 -0500

    Make routes simpler

--- poll-service/src/main/java/io/spring/sample/poll/controller/PollController.java
+++ poll-service/src/main/java/io/spring/sample/poll/controller/PollController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/polls")
+@RequestMapping("/")
 public class PollController {
 
     private static final Logger log = LoggerFactory.getLogger(PollController.class);
--- results-service/src/main/java/io/spring/sample/results/controller/ResultsController.java
+++ results-service/src/main/java/io/spring/sample/results/controller/ResultsController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/results")
+@RequestMapping("/")
 public class ResultsController {
 
     private final ResultsAggregator resultsAggregator;
